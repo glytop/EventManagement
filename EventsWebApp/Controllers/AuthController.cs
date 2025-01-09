d04ce4fc78629cbac24f289f6a2e41ebe83934f2
glytop <123397208+glytop@users.noreply.github.com>
2025-01-09 23:23:33 +0300

    add folder

---
+++ EventsWebApp/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using EventsWebApp.Domain.Entities;
+using EventsWebApp.Infrastructure.Services;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventsWebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private JwtTokenService _jwtTokenService;
+
+        private static List<User> Users = new()
+    {
+        new User
+        {
+            Id = 1,
+            Username = "admin",
+            Email = "admin@example.com",
+            PasswordHash = "admin",
+            Roles = new List<string> { "Admin" }
+        }
+    };
+
+        public AuthController(JwtTokenService jwtTokenService)
+        {
+            _jwtTokenService = jwtTokenService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(Domain.Entities.LoginRequest request)
+        {
+            var user = Users.FirstOrDefault(u => u.Username == request.Username && u.PasswordHash == request.Password);
+
+            if (user == null)
+            {
+                return Unauthorized(new { Message = "Invalid credentials" });
+            }
+
+            var token = _jwtTokenService.GenerateToken(user);
+            return Ok(new { Token = token });
+        }
+    }
+}
---
+++ EventsWebApp/Infrastructure/Services/JwtTokenService.cs
@@ -0,0 +1,45 @@
+﻿using EventsWebApp.Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace EventsWebApp.Infrastructure.Services
+{
+    public class JwtTokenService
+    {
+        private IConfiguration _configuration;
+
+        public JwtTokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(User user)
+        {
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+
+            var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, user.Username),
+            new Claim(ClaimTypes.Email, user.Email)
+        };
+
+            // Добавление ролей пользователя в токен
+            claims.AddRange(user.Roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSettings["Issuer"],
+                audience: jwtSettings["Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(jwtSettings["ExpiresInMinutes"])),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
